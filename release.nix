62f8d409d5dba11ef2d90b416a7bbcc1c6c73555
knupfer <fknupfer@gmail.com>
2019-02-10 21:58:14 +0100

    add terminal-size

--- release.nix
+++ release.nix
@@ -4,7 +4,6 @@ with nixpkgs.haskell.lib;
 with nixpkgs.lib;
 let tested = [ "ghc844"
                "ghc863"
-               "ghcHEAD"
                ];
     eval = x: import ./default.nix { nixpkgs = nixpkgs; compiler = x; };
 in
