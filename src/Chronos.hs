80350933e514ca86dd4fb07e90f867e2f47fd288
knupfer <fknupfer@gmail.com>
2019-02-21 10:16:43 +0100

    major cleanup

--- src/Chronos.hs
+++ src/Chronos.hs
@@ -1,11 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 
 module Chronos
@@ -41,8 +34,8 @@ import System.Process
 import System.Console.Terminal.Size
 import System.Mem
 
-import qualified Data.Set as S
 import qualified Data.ByteString.Builder as B
+import qualified Data.Set as S
 
 data Benchmark
   = Benchmark
@@ -54,6 +47,7 @@ data Benchmark
 data BenchmarkMeta
   = BenchmarkMeta
   { information :: Double
+  , maxDuration :: Rational
   , position :: Int
   , benchmark :: Benchmark
   }
@@ -62,7 +56,7 @@ instance Eq BenchmarkMeta where
   (==) = (==) `on` position &&& analysis . benchmark
 
 instance Ord BenchmarkMeta where
-  compare = compare `on` information &&& position &&& analysis . benchmark
+  compare = compare `on` information &&& negate . position &&& analysis . benchmark
 
 data Analysis
   = Analysis
@@ -73,35 +67,45 @@ data Analysis
   , variance :: Rational
   } deriving (Eq, Ord, Show, Read)
 
+data Computation where
+   Shell :: String -> Computation
+   Pure :: NFData b => (a -> b) -> a -> Computation
+   Impure :: IO a -> Computation
+
+printBenchmark :: BenchmarkMeta -> IO ()
+printBenchmark b = do
+  w <- maybe 60 width <$> size
+  B.hPutBuilder stdout $ csi' [3*position b-1] 'F' <> renderBenchmark w (maxDuration b) (benchmark b) <> csi' [3*position b-2] 'E'
+
+renderBenchmark :: Int -> Rational -> Benchmark -> B.Builder
+renderBenchmark w maxDuration Benchmark{..}
+  = B.char7 ' '
+  <> B.char7 ' '
+  <> renderAnalysis analysis
+  <> csi' [0] 'K'
+  <> B.char7 '\n'
+  <> if samples analysis <= 1
+  then mempty
+  else barBuilder w (mean analysis / maxDuration) (min 1 $ sigmaLevel * stdError analysis / fromRational (mean analysis)) (min 1 $ sigma analysis / fromRational (mean analysis))
+
 defaultMain :: [Benchmark] -> IO ()
 defaultMain bs = bracket_ hideCursor showCursor $
-  B.hPutBuilder stdout (foldMap (nameBuilder . name) bs <> B.char7 '\n')
-  *> runMain (S.fromList $ zipWith (BenchmarkMeta 0) [1..] bs)
+  B.hPutBuilder stdout (foldMap (nameBuilder . name) bs)
+  *> foldr1 (>=>) (replicate 10 step) (benchIO "warmup" (return ()))
+  *> runMain (S.fromList $ zipWith (BenchmarkMeta 0 0) [1..] $ reverse bs)
 
 runMain :: S.Set BenchmarkMeta -> IO ()
 runMain = fix (go>=>) . (,) 0
   where
-    go (r,s) = case S.minView s of
-      Just (BenchmarkMeta{..}, s') ->
-            let mv = (length s - position) * 3 + 3
-            in do
-          B.hPutBuilder stdout
-            $ csi' [mv] 'F' -- cursorUpLine mv
-            <> indicatorBuilder
-            <> csi' [mv] 'E' -- cursorDownLine mv
+    go (md, s) = case S.minView s of
+      Just (BenchmarkMeta{..}, s') -> do
           ana <- analysis <$> step benchmark
-          let newMax | r == mean (analysis benchmark) = mean ana
-                     | otherwise = max r $ mean ana
-          w <- maybe 60 width <$> size
-          B.hPutBuilder stdout
-            $ csi' [mv] 'F' -- cursorUpLine mv
-            <> analysisBuilder (renderAnalysis ana)
-            <> case () of
-                 _ | samples ana <= 1 -> B.char7 '\n'
-                   | otherwise  -> barBuilder w (mean ana / newMax) (min 1 $ sigmaLevel * stdError ana / fromRational (mean ana)) (min 1 $ sigma ana / fromRational (mean ana))
-            <> csi' [mv-2] 'E' -- cursorDownLine $ mv-2
-          pure (newMax, S.insert (BenchmarkMeta (informationOf ana) position benchmark{analysis = ana}) s')
-      Nothing -> pure (r,s)
+          let newMax | md == mean (analysis benchmark) = mean ana
+                     | otherwise = max md $ mean ana
+              new = BenchmarkMeta (informationOf ana) newMax position benchmark{analysis = ana}
+          printBenchmark new *> pure (newMax, S.insert new s')
+
+      Nothing -> pure (md, s)
 
 {-# INLINE step #-}
 step :: Benchmark -> IO Benchmark
@@ -110,11 +114,6 @@ step (Benchmark n a f) = flip (Benchmark n) f <$> f a
 {-# INLINE benchIO #-}
 benchIO :: String -> IO a -> Benchmark
 benchIO label io = runComputation (Impure io) label
-
-data Computation where
-   Shell :: String -> Computation
-   Pure :: NFData b => (a -> b) -> a -> Computation
-   Impure :: IO a -> Computation
 
 {-# INLINE measure #-}
 measure :: (Int -> IO a) -> Analysis -> IO Analysis
@@ -130,7 +129,9 @@ runComputation :: Computation -> String -> Benchmark
 runComputation comp label = Benchmark label (Analysis 0 0 0 0 0) $ case comp of
   Impure io -> measure (flip replicateM_ io)
   Pure g x  -> \ana -> newIORef x >>= \io -> (measure (\n -> replicateM_ n $ (return$!) . force . g =<< readIORef io) ana)
-  Shell cmd -> measure (\n -> withCreateProcess (shell (intercalate ";" $ replicate n cmd)) {std_out = CreatePipe, std_err = CreatePipe} $ \_ _ _ -> void . waitForProcess)
+  Shell cmd -> measure go
+    where go n = uncurry (>>) $ (flip replicateM_ (f 10000) *** f) (n `divMod` 10000)
+          f x = withCreateProcess (shell (intercalate ";" $ replicate x cmd)) {std_out = CreatePipe, std_err = CreatePipe} $ \_ _ _ -> void . waitForProcess
 
 {-# INLINE benchShell #-}
 benchShell :: String -> String -> Benchmark
@@ -143,6 +144,8 @@ bench label f x = runComputation (Pure f x) label
 {-# INLINE renderAnalysis #-}
 renderAnalysis :: Analysis -> B.Builder
 renderAnalysis a@Analysis{..}
+  | samples == 0 = mempty
+  | otherwise
   = B.char7 't' <> B.char7 '='
   <> prettyScientific (fromRational mean) (Just $ sigmaLevel * stdError a)
   <> B.char7 's' <> B.char7 ' '
@@ -154,8 +157,7 @@ renderAnalysis a@Analysis{..}
 
 compareBench :: Double -> Benchmark -> Benchmark -> IO Ordering
 compareBench d b1 b2 | ((||) `on` (<3) . samples . analysis) b1 b2
-                       || ((||) `on` (<1) . informationOf . analysis) b1 b2
-                         = next
+                       || ((||) `on` (<1) . informationOf . analysis) b1 b2 = next
                      | otherwise = case compareMeans (analysis b1) (analysis b2) of
                          EQ | ((||) `on` (not . relativeErrorBelow (d/2) . analysis)) b1 b2 -> next
                          r -> pure r
@@ -197,7 +199,6 @@ prettyScientific x b = case floatToDigits 10 <$> b of
     _ | x == 0 -> B.char7 '0'
     _ -> mantissa (take 2 $ sig ++ repeat 0) <> f expo
   where
-
     showError err = B.char7 '(' <> foldMap B.intDec (take 2 $ err ++ repeat 0) <> B.char7 ')'
     (sig,expo) = floatToDigits 10 x
     valLen e = expo - e + 2
@@ -210,26 +211,12 @@ prettyScientific x b = case floatToDigits 10 <$> b of
 
 showE :: Integral a => a -> B.Builder
 showE = fix go
-  where go f = \case
-          0 -> B.charUtf8 '⁰'
-          1 -> B.charUtf8 '¹'
-          2 -> B.charUtf8 '²'
-          3 -> B.charUtf8 '³'
-          4 -> B.charUtf8 '⁴'
-          5 -> B.charUtf8 '⁵'
-          6 -> B.charUtf8 '⁶'
-          7 -> B.charUtf8 '⁷'
-          8 -> B.charUtf8 '⁸'
-          9 -> B.charUtf8 '⁹'
-          n | n < 0 -> B.charUtf8 '⁻' <> f (abs n)
-            | otherwise -> case divMod n 10 of
-                (0, b) -> f b
-                (a, b) -> f a <> f b
+  where go f n | n < 0 = B.charUtf8 '⁻' <> f (abs n)
+               | n < 10 = B.charUtf8 $ "⁰¹²³⁴⁵⁶⁷⁸⁹" !! fromIntegral n
+               | otherwise = uncurry ((<>) `on` f) $ divMod n 10
 
 sigma :: Analysis -> Double
-sigma Analysis{..}
-  = sqrt (fromRational variance)
-  / biasCorrection
+sigma Analysis{..} = sqrt (fromRational variance) / biasCorrection
   where biasCorrection
           = 1
           - 1/(4*fromIntegral samples)
@@ -237,14 +224,13 @@ sigma Analysis{..}
           - 19/(128*fromIntegral samples**3)
 
 stdError :: Analysis -> Double
-stdError a@Analysis{..}
-   = sigma a * sqrt (fromIntegral squaredWeights) / fromIntegral samples
+stdError a@Analysis{..} = sigma a * sqrt (fromIntegral squaredWeights) / fromIntegral samples
 
 informationOf :: Analysis -> Double
 informationOf Analysis{..} = sqrt (fromRational mean) * fromIntegral samples
 
 weightOf :: Analysis -> Int
-weightOf Analysis{..} = fromIntegral . max 1 . min samples . round . recip $ sqrt (fromRational mean :: Double)
+weightOf Analysis{..} = fromIntegral . max 1 . min samples . round . recip $ (fromRational mean :: Double) ** 0.7
 
 {-# INLINE refineAnalysis #-}
 refineAnalysis :: Analysis -> SystemTime -> SystemTime -> Analysis
@@ -257,9 +243,7 @@ refineAnalysis ana@Analysis{..} begin end = Analysis newSamples newSquaredWeight
     newQFactor = qFactor + diffWeight * (time - newMean)
     newVariance | newSamples > 1 = newQFactor / fromIntegral (newSamples - 1)
                 | otherwise = 0
-
     diffWeight = fromIntegral (weightOf ana) * (time - mean)
-
     time = (toSeconds end - toSeconds begin) / fromIntegral (weightOf ana)
     toSeconds t = fromIntegral (systemSeconds t) + fromIntegral (systemNanoseconds t) / 1e9
 
@@ -270,21 +254,9 @@ csi' :: [Int] -> Char -> B.Builder
 csi' (x:xs) b = B.char7 '\ESC' <> B.char7 '[' <> B.intDec x <> foldMap (\n -> B.char7 ';' <> B.intDec n) xs <> B.char7 b
 csi' [] b = B.char7 '\ESC' <> B.char7 '[' <> B.char7 b
 
-indicatorBuilder :: B.Builder
-indicatorBuilder = sgrBuilder (SetColor Foreground Vivid Red) <> B.charUtf8 '►' <> sgrBuilder (Reset)
-
-analysisBuilder :: B.Builder -> B.Builder
-analysisBuilder result = do
-    csi' [2] 'K' -- clearLine
-    <> B.char7 ' '
-    <> B.char7 ' '
-    <> result
-    <> B.char7 '\n'
-
 barBuilder :: Int -> Rational -> Double -> Double -> B.Builder
 barBuilder width m stdErr sd =
-  csi' [2] 'K' -- clearLine
-  <> B.char7 ' ' <> B.char7 ' ' <> B.stringUtf8 (replicate (pred valueLength) '▀')
+  B.char7 ' ' <> B.char7 ' ' <> B.stringUtf8 (replicate (pred valueLength) '▀')
   <> sgrBuilder (SetColor Foreground Dull Magenta)
   <> B.stringUtf8 (replicate errorLength '▀')
   <> middle
@@ -293,13 +265,13 @@ barBuilder width m stdErr sd =
   <> sgrBuilder (SetColor Foreground Vivid Black)
   <> B.stringUtf8 (replicate sigmaLength '▔')
   <> sgrBuilder Reset
-  <> B.char7 '\n'
+  <> csi' [0] 'K' -- clearLine
 
   where
     middle
       | len * stdErr >= 0.20 = sgrBuilder (SetColor Foreground Vivid Magenta) <> B.charUtf8 '▀'
       | otherwise = mempty
-    len = fromRational m * fromIntegral (width - 2) / 2
+    len = fromRational m * fromIntegral (width - 6) / 2
     valueLength = round len - errorLength
     errorLength = round $ len * stdErr
     sigmaLength = round (len * sd) - errorLength
@@ -309,4 +281,4 @@ nameBuilder n =
   sgrBuilder (SetColor Foreground Vivid Cyan)
   <> fromString n
   <> sgrBuilder Reset
-  <> "\n\n\n"
+  <> fromString "\n\n\n"
