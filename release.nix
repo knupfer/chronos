52df9775569e03574c66aebd2368a0f978f04d98
knupfer <fknupfer@gmail.com>
2019-02-24 21:34:43 +0100

    add two tested ghc versions

--- release.nix
+++ release.nix
@@ -3,6 +3,8 @@
 with nixpkgs.haskell.lib;
 with nixpkgs.lib;
 let tested = [ "ghc844"
+               "ghc861"
+               "ghc862"
                "ghc863"
                ];
     eval = x: import ./default.nix { nixpkgs = nixpkgs; compiler = x; };
